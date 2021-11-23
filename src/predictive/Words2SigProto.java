1725fbc3ebd3d812ef27fd4e45e9916f2f5f1203
Muhammad Hidayat <hidayat.pcd@gmail.com>
2021-11-23 19:19:17 +0800

    Implement words to signature
    Using StringBuilder for performance, reasons TBA.

--- src/predictive/Words2SigProto.java
+++ src/predictive/Words2SigProto.java
@@ -1,10 +1,33 @@
 package predictive;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Words2SigProto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		ArrayList<String> l;
+		// Check if empty
+		if (args.length == 0) {
+			l = new ArrayList<>();
+			// read from stdin
+			Scanner in = new Scanner(System.in);
+			while (in.hasNext()) {
+				l.add(in.next());
+			}
+			in.close();
+		} else {
+			l = new ArrayList<>(Arrays.asList(args));
+		}
+		
+		// only process if not empty
+		// It can still be empty from empty stdin
+		if (l.size() != 0) {
+			for (String s: l) {
+				System.out.println(PredictivePrototype.wordToSignature(s));
+			}
+		}
 	}
 
 }
